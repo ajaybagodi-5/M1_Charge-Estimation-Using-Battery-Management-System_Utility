2eef8a89819f7c6181f5530cf238a6712d610e37
ajaybagodi-5 <100822443+ajaybagodi-5@users.noreply.github.com>
2022-03-03 14:18:10 +0530

    Update test_BMS.c

--- 3_Implementation/SRC/test_BMS.c
+++ 3_Implementation/SRC/test_BMS.c
@@ -1,1 +1,53 @@
+/**
+ * @file tes_BMS_.c
+ * @author ajay bagodi(you@domain.com)
+ * @brief Unit testing
+ * @version 0.1
+ * @date 2022-02-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include"unity.h"
+#include"BMS.h"
+
+void push(st*s,float newitem){
+    if(isfull(s)){
+        printf("STACKFULL");
+    }else{
+        s->top++;
+        s->items[s->top]=newitem;
+    }
+     count++;
+}
+void pop(st*s){
+    if(isempty(s)){
+        printf("\nSTACK EMPTY\n");
+    }else{
+        printf("Item popped=%d",s->items[s->top]);
+        s->top--;
+
+    }
+    count--;
+    printf("\n");
+
+}
+void printstack(st*s){
+    printf("Stack:");
+    for(int i=0;i<count;i++){
+        printf("%d-->",s->items[i]);
+    }
+    printf("\n");
+
+
+}
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_loadcurrent);
+    RUN_TEST(test_reamining percentage);
+    return UNITY_END();
+}
+
+
 
