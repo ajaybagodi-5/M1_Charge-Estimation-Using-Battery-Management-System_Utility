2e7ddd89b7a4c38dc3ffd25eff37de711b68b4e1
ajaybagodi-5 <100822443+ajaybagodi-5@users.noreply.github.com>
2022-03-03 12:41:32 +0530

    updated

--- 3_Implementation/SRC/BMS.c
+++ 3_Implementation/SRC/BMS.c
@@ -1,1 +1,78 @@
+#include "BMS.h"
 
+int count=0;
+
+struct stack
+{
+    int items[MAX];
+    int top;
+};
+typedef struct stack st;
+
+void createEmptyStack(st*s)    //function definition
+{
+    s->top=-1;
+}
+int isfull(st*s)
+{
+    if(s->top==MAX-1)
+        return 1;
+    else
+        return 0;
+}
+
+int isempty(st*s)
+{
+    if(s->top==-1)
+        return 1;
+    else
+        return 0;
+}
+void push(st*s,float newitem)
+{
+    if(isfull(s)){
+        printf("STACKFULL");
+    }
+    else
+    {
+        s->top++;
+        s->items[s->top]=newitem;
+    }
+     count++;
+
+
+}
+
+void pop(st*s)
+{
+    if(isempty(s)){
+        printf("\nSTACK EMPTY\n");
+    }
+    else
+    {
+        printf("Item popped=%d",s->items[s->top]);
+        s->top--;
+
+    }
+    count--;
+    printf("\n");
+
+}
+
+
+
+
+void printstack(st*s)
+{
+    printf("Stack:");
+    for(int i=0;i<count;i++){
+        printf("%d-->",s->items[i]);
+    }
+    printf("\n");
+
+
+}
+double i(double t)
+{
+    return(t/pow(t,2)+1);
+}
