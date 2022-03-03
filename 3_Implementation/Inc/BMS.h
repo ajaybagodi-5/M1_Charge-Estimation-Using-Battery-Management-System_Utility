409c35fa8b50369d93de649174d52476a24c7037
ajaybagodi-5 <100822443+ajaybagodi-5@users.noreply.github.com>
2022-03-03 12:34:54 +0530

    updated

--- 3_Implementation/Inc/BMS.h
+++ 3_Implementation/Inc/BMS.h
@@ -1,1 +1,34 @@
+/**
+ * @file BMS.h
+ * @author Ajay Bagodi
+ * @brief this project is about BMS(Battery Management System)
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef BMS_H_
+#define BMS_H_
+
+#include<stdio.h>
+#include<math.h> 
+#include<conio.h> 
+#define MAX 10
+
+
+int Load_current(int Voc,float Load,int i_t) 
+
+/**
+ * @brief Here this function is going to calculate load current of the battery
+ * 
+ * @return int
+ */
+int Remaining_percentage(int SOCt1,float i_t,int MUn,float Time,int SOCt)
+
+/**
+ * @brief Here this function is going to calculate the SOC of the battery and gives remaining percentage
+ * 
+ */
+#endif /* D89ECC6E_184A_4F0E_BBBA_9ED3CF9A7923 */
 
