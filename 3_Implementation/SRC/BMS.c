5ecf8fbcee3d73d8e7d2770ab260f55ecdc44948
ajaybagodi-5 <100822443+ajaybagodi-5@users.noreply.github.com>
2022-03-03 13:42:27 +0530

    Update BMS.c

--- 3_Implementation/SRC/BMS.c
+++ 3_Implementation/SRC/BMS.c
@@ -1,78 +1,16 @@
 #include "BMS.h"
 
-int count=0;
 
-struct stack
-{
-    int items[MAX];
-    int top;
-};
-typedef struct stack st;
 
-void createEmptyStack(st*s)    //function definition
+int Load_current(int Voc,float Load,int i_t)   //function definition
 {
-    s->top=-1;
-}
-int isfull(st*s)
-{
-    if(s->top==MAX-1)
-        return 1;
-    else
-        return 0;
+    i_t=Voc/Load;
+    return i_t;
 }
 
-int isempty(st*s)
+int Remaining_percentage(int SOCt1,float i_t,int MUn,float Time,int SOCt)
 {
-    if(s->top==-1)
-        return 1;
-    else
-        return 0;
-}
-void push(st*s,float newitem)
-{
-    if(isfull(s)){
-        printf("STACKFULL");
-    }
-    else
-    {
-        s->top++;
-        s->items[s->top]=newitem;
-    }
-     count++;
+    SOCt=SOCt1+(-(i_t/MUn)*Time)*100;
+    return SOCt;
 
-
-}
-
-void pop(st*s)
-{
-    if(isempty(s)){
-        printf("\nSTACK EMPTY\n");
-    }
-    else
-    {
-        printf("Item popped=%d",s->items[s->top]);
-        s->top--;
-
-    }
-    count--;
-    printf("\n");
-
-}
-
-
-
-
-void printstack(st*s)
-{
-    printf("Stack:");
-    for(int i=0;i<count;i++){
-        printf("%d-->",s->items[i]);
-    }
-    printf("\n");
-
-
-}
-double i(double t)
-{
-    return(t/pow(t,2)+1);
 }
